b59bc80f223f3e8a9cc8a5ac55642c31649763ec
eddyizm <eddyizm@gmail.com>
2019-07-12 22:15:02 -0700

    scaffolding new python scripts to house data access functions and validation for future licensing

---
+++ FlatFileExporter/PythonCodeBase/DAL.py
@@ -0,0 +1,4 @@
+''' Data Access Layer
+Main library to access database, read sql scripts, and generate files. 
+Will also use this area to check for ODBC.
+'''
---
+++ FlatFileExporter/PythonCodeBase/ffe_validation.py
@@ -0,0 +1,3 @@
+''' library to build validation for trail period and possibly licensing. 
+will use this to build sqlite db and encrypt for usage in c# portion
+'''
