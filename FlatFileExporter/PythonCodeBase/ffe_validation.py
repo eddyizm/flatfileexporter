cd7ba7620942e7f693a8becb3ba7d5a5e927cc85
eddyizm <eddyizm@gmail.com>
2019-07-13 16:50:53 -0700

    create table db and  sql statements added to ffe_val

--- FlatFileExporter/PythonCodeBase/ffe_validation.py
+++ FlatFileExporter/PythonCodeBase/ffe_validation.py
@@ -5,13 +5,37 @@ import requests
 import os
 import sqlite3
 
+current_directory= os.getcwd() # this may change before beta/alpha testing.
+
 # link to check https://www.instagram.com/p/voLqUzoTVk/?igshid=6astge42ug6y
 
 def check_url():
-  r = requests.get('https://www.instagram.com/p/voLqUzoTVk/')
-  # print(r.status_code)
-  # print('true' if r.status_code==200 else 'false')
-  return True if r.status_code==200 else False
+    r = requests.get('https://www.instagram.com/p/voLqUzoTVk/')
+    # print(r.status_code)
+    # print('true' if r.status_code==200 else 'false')
+    return True if r.status_code==200 else False
+
+# TODO Get current date and store in sqlite3 for 30 day trial. 
+''' sqlite3 statements '''
+create_validation_table = """ CREATE TABLE IF NOT EXISTS validation_date (
+                                        id integer PRIMARY KEY,
+                                        current_date text,
+                                        start_date text
+                                    ); """		
+
+def create_connection(db_file):
+    """ create a database connection to the SQLite database
+        specified by db_file
+    :param db_file: database file
+    :return: Connection object or None
+    """
+    try:
+        conn = sqlite3.connect(db_file)
+        return conn
+    except Error as e:
+        print(e)
+ 
+    return None  
 
 if __name__ == '__main__':
   # pass
