34dbd2bdca1270c21451c13d270988a5abd03798
eddyizm <eddyizm@gmail.com>
2019-07-13 09:46:55 -0700

    added import statements and main with todos in DAL

--- FlatFileExporter/PythonCodeBase/DAL.py
+++ FlatFileExporter/PythonCodeBase/DAL.py
@@ -2,3 +2,19 @@
 Main library to access database, read sql scripts, and generate files. 
 Will also use this area to check for ODBC.
 '''
+import pyodbc as db
+import pandas as pd
+import os
+from datetime import datetime
+date_var = datetime.now().strftime("%Y%m%d")
+# TODO When called from UI the directory will default to the location of the SQL script. 
+directory = '' 
+
+
+if __name__ == '__main__':
+  main()
+  # TODO Add version 
+  # TODO Add logging
+  # TODO check for ODBC drivers
+  
+  
