08d243cad843565f36a1b66308b2caa63a7a8f59
eddyizm <eddyizm@gmail.com>
2019-07-14 09:17:30 -0700

    tested print args

--- FlatFileExporter/PythonCodeBase/flatfile_cli.py
+++ FlatFileExporter/PythonCodeBase/flatfile_cli.py
@@ -15,13 +15,28 @@ parser.add_argument("server", help="server to run query against")
 parser.add_argument("db", help="database to use on server")
 parser.add_argument("directory", help="location to export flat file. do not specify extension.")
 parser.add_argument("filename", help="filename. date will be appended")
+args=parser.parse_args()
 
 current_directory= os.getcwd() # this may change before beta/alpha testing.
 
+def get_seperator(*, seperator):
+    ''' get file seperator from GUI or CLI and pass the correct value to function '''
+    pass
+
+def main():
+    # debugging CLI arg inputs
+    print(args.server)
+    print(args.db)
+    print(args.directory)
+    print(args.filename)
+
 if __name__ == '__main__':
-    print('testing ffe validation')
+    main()
+    # print('testing check odbc')
     # print(db.check_odbc())
-    print(current_directory)
-    print(ffe_val.db_store)
+    # print('testing ffe validation')
+    # print(current_directory)
+    # print(ffe_val.db_store)
 
     os.system("pause")
+    
