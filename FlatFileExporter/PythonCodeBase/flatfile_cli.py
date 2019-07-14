c1b3203994dbe0e3b2d15150c9f48317e551015f
eddyizm <eddyizm@gmail.com>
2019-07-14 09:07:31 -0700

    added arg parser

--- FlatFileExporter/PythonCodeBase/flatfile_cli.py
+++ FlatFileExporter/PythonCodeBase/flatfile_cli.py
@@ -9,6 +9,13 @@ import ffe_validation as ffe_val # Local Library
 import argparse
 import sys
 
+# argument parser
+parser = argparse.ArgumentParser(description="Takes sql script or stored proc to export csv, txt or excel file.")
+parser.add_argument("server", help="server to run query against")
+parser.add_argument("db", help="database to use on server")
+parser.add_argument("directory", help="location to export flat file. do not specify extension.")
+parser.add_argument("filename", help="filename. date will be appended")
+
 current_directory= os.getcwd() # this may change before beta/alpha testing.
 
 if __name__ == '__main__':
