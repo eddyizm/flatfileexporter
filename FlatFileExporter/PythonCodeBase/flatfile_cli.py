dae56bac11e36d55dc645e9f12c45574d450835f
eddyizm <eddyizm@gmail.com>
2019-07-19 22:13:59 -0700

    added sql arguments

--- FlatFileExporter/PythonCodeBase/flatfile_cli.py
+++ FlatFileExporter/PythonCodeBase/flatfile_cli.py
@@ -12,15 +12,20 @@ import sys
 # argument parser
 parser = argparse.ArgumentParser(description="Takes sql script or stored proc to export csv, txt or excel file.")
 parser.add_argument("-V", "--version", action='version', version =f'v{db.__version__}')
-parser.add_argument("server", help="server to run query against")
-parser.add_argument("db", help="database to use on server")
+parser.add_argument("server", help="server to run query against.")
+parser.add_argument("db", help="database to use on server.")
 parser.add_argument("directory", help="location to export flat file. do not specify extension.")
-parser.add_argument("filename", help="filename. date will be appended")
+parser.add_argument("filename", help="filename. Date will be appended/")
 # first group for extension
 group_seperator = parser.add_mutually_exclusive_group(required=True)
-group_seperator.add_argument("-c", "--comma", action='store_const', const=',', help="use comma characterfor seperator")
-group_seperator.add_argument("-t", "--tab", action='store_const', const='t', help="uses tab for seperator")
-group_seperator.add_argument("-p", "--pipe", action='store_const', const='|', help="uses pipe character for seperator")
+group_seperator.add_argument("-c", "--comma", action='store_const', const=',', help="use comma characterfor seperator.")
+group_seperator.add_argument("-t", "--tab", action='store_const', const='t', help="uses tab for seperator.")
+group_seperator.add_argument("-p", "--pipe", action='store_const', const='|', help="uses pipe character for seperator.")
+# second group for sql script or stored proc
+group_sql = parser.add_mutually_exclusive_group(required=True)
+group_sql.add_argument("-s", "--sqlscript",  help="full path of sql script *MUST HAVE* SET NO COUNT on script.")
+group_sql.add_argument("-sp", "--storedproc", help="instead of sql script, point to stored procedure on database. Wrap in double quotes with parameters eg. \"EXEC SAMPLE_SP '<Param>'\"")
+
 args=parser.parse_args()
 
 current_directory= os.getcwd() # this may change before beta/alpha testing.
