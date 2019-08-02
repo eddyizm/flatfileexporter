787ea0f853a10be524e464db4dc7fe608651ea33
eddyizm <eddyizm@gmail.com>
2019-08-01 21:46:13 -0700

    added credential buttons. saving and setting username

--- FlatFileExporter/CredentialsPage.xaml.cs
+++ FlatFileExporter/CredentialsPage.xaml.cs
@@ -23,11 +23,54 @@ namespace FlatFileExporter
         public CredentialsPage()
         {
             InitializeComponent();
+            LoadCredentials();
+        }
+
+        private void LoadCredentials()
+        {
+            try
+            {
+                if (!string.IsNullOrEmpty(Properties.Settings.Default.Username))
+                {
+                    txUserName.Text = Properties.Settings.Default.Username;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+            
         }
 
         private void ExUserPass_Expanded(object sender, RoutedEventArgs e)
         {
             exTrustConnnect.IsExpanded = false;
+        }
+
+        private void btnClear_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mw = Application.Current.Windows.OfType<MainWindow>().FirstOrDefault();
+            if (mw != null)
+            {
+                mw._mainFrame.NavigationService.RemoveBackEntry();
+                mw._mainFrame.Content = new MainPage();
+            }
+        }
+
+        private void btnSaveCred_Click(object sender, RoutedEventArgs e)
+        {
+
+            try
+            {
+                Properties.Settings.Default.Username = txUserName.Text;
+                //Properties.Settings.Default.Password = txPassword.Text;
+                Properties.Settings.Default.Save();
+                
+            }
+            catch (NullReferenceException ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
         }
     }
 }
