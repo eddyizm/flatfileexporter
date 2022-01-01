fe0f982d78b787ba6f874a591cb7791d8c63375e
eddyizm <eddyizm@gmail.com>
2022-01-01 14:59:26 -0800

    added support for two more drivers. Tested successfully  'SQL Server Native Client 11.0', 'SQL Server' #3

--- FlatFileExporter/PythonCodeBase/DAL.py
+++ FlatFileExporter/PythonCodeBase/DAL.py
@@ -6,7 +6,7 @@ import pyodbc as db
 import pandas as pd
 import os
 from datetime import datetime
-import xlsxwriter
+# import xlsxwriter
 import logging
 log = logging.getLogger('root')
 
@@ -15,11 +15,13 @@ date_var = datetime.now().strftime("%Y%m%d")
 # TODO figure out how to auto increment this and pass it/share it with c# codebase
 __version__= '0.1.5'
 
-DB_STRING = 'DRIVER={{ODBC Driver 17 for SQL Server}};SERVER={};Trusted_Connection=yes;DATABASE={}'	
-USERPASS_DBSTRING = 'DRIVER={{ODBC Driver 17 for SQL Server}};SERVER={};DATABASE={};UID={};PWD={}'
+DB_STRING = 'DRIVER={{{}}};SERVER={};Trusted_Connection=yes;DATABASE={}'	
+USERPASS_DBSTRING = 'DRIVER={{{}}};SERVER={};DATABASE={};UID={};PWD={}'
 ODBC_MISSING_MSG = '''ODBC driver not found
 please install ODBC Driver 17 for SQL Server
 https://www.microsoft.com/en-us/download/details.aspx?id=56567'''
+MSDRIVERS = ['ODBC Driver 17 for SQL Server', 'ODBC Driver 13 for SQL Server',
+   'SQL Server Native Client 11.0', 'SQL Server']
 
 
 def check_odbc() -> bool:
@@ -30,8 +32,11 @@ def check_odbc() -> bool:
       log.info('Checking for \'ODBC Driver 17 for SQL Server\' driver')
       for driver in drivers:
         if driver == 'ODBC Driver 17 for SQL Server':
-          log.info('Driver found')
-          return True
+            log.info(f'Driver found - {driver}')
+            return True
+        if driver == 'ODBC Driver 11 for SQL Server':
+            log.info(f'Driver found - {driver}')
+            return True
       else:
         log.warn(ODBC_MISSING_MSG)
         return False    
@@ -40,6 +45,23 @@ def check_odbc() -> bool:
       return False
   except Exception as ex:
     log.error(f'Error in check_odbc(): {ex}')
+
+
+def get_odbc() -> str:
+  ''' get odbc driver - the check should have already been done and pass
+  however maybe I need to combine this later to specify which one to use.
+  '''
+  try:
+    drivers = db.drivers()
+    if drivers:
+      log.info('Loading driver')
+      for driver in MSDRIVERS:
+        if driver in drivers:
+          log.info(f'Driver found - {driver}')
+          return driver
+
+  except Exception as ex:
+    log.error(f'Error in get_odbc(): {ex}')
 
 
 def read_file(file):
@@ -58,12 +80,13 @@ def read_file(file):
 def generate_file(query, db_name, server, separator, directory, extension, file_name=None, username=None, password=None):
   ''' generate file magic '''
   log.info('Generating file')
+  driver = get_odbc()
   # defaulting to no username and going to trusted connection. Should be the setup in most enterprise shops
   if not username:
     log.debug('No username received, using trusted connection (MSSQL)')
-    db_connection=DB_STRING.format(server, db_name)
+    db_connection=DB_STRING.format(driver, server, db_name)
   else:
-    db_connection = USERPASS_DBSTRING.format(server, db_name, username, password)
+    db_connection = USERPASS_DBSTRING.format(driver, server, db_name, username, password)
   # passing the name supplied otherwise defaulting but this may not work properly. Need to test in other environments.
   if file_name:
     filename = (file_name + '_' + date_var + extension)
@@ -89,7 +112,7 @@ def generate_file(query, db_name, server, separator, directory, extension, file_
   
 
 if __name__ == '__main__':
-  # main()
+  check_odbc()
   pass
   # TODO Add version 
   
