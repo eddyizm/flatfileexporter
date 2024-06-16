27b87f25147ed1e04862c81b7cc86528fd9d1fb5
eddyizm <eddyizm@gmail.com>
2024-06-15 22:55:35 -0700

    wip: more layout

--- src/DAL.py
+++ src/DAL.py
@@ -115,6 +115,3 @@ if __name__ == '__main__':
   check_odbc()
   pass
   # TODO Add version 
-  
-  
-  
--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 from textual.app import App, ComposeResult
 from textual.widgets import (
     Button,
+    Checkbox,
     Footer,
     Header,
     Input,
@@ -9,11 +10,25 @@ from textual.widgets import (
 )
 from textual.containers import (
     Container,
-    Grid
+    Grid,
+    Horizontal
 )
 
 # #313245
 # tuna
+class FileExtensionCheckbox(Static):
+
+    def compose(self) -> ComposeResult:
+        with Horizontal():
+            yield Checkbox("csv", True)
+            yield Checkbox("[magenta]txt[/]")
+            yield Checkbox("[b]XLSX[/b]")
+
+class ConnectionSettings(Static):
+    # TODO enter connection settings, save to file?
+    # encrypt or obfuscate creds?
+    pass
+
 
 class FlatFileExporterApp(App):
     """A Textual app export flat files."""
@@ -21,21 +36,32 @@ class FlatFileExporterApp(App):
     CSS_PATH = "styling.tcss"
     BINDINGS = [
         ("d", "toggle_dark", "Toggle dark mode"),
+        ("ctrl+q", "quit", "Quit")
     ]
 
+    def on_button_pressed(self, event: Button.Pressed) -> None:
+        """Event handler called when a button is pressed."""
+        button_id = event.button.id
+        if button_id == "exit":
+            self.exit()
+ 
     def compose(self) -> ComposeResult:
         """Create child widgets for the app."""
         yield Header()
         yield Label('top level text header', classes='box')
-        yield Input()
+        yield FileExtensionCheckbox(classes='box')
         yield Footer()
         with Container(classes='box'):
+            yield Input(placeholder='Select a sql script...')
             yield Button(label='Generate File')
-            yield Button(label='Exit')
+            yield Button(id='exit', label='Exit')
 
     def action_toggle_dark(self) -> None:
         """An action to toggle dark mode."""
         self.dark = not self.dark
+
+    def action_quit(self) -> None:
+        self.exit()
 
 
 if __name__ == "__main__":
