eb8086b2486db5715d5dde2e0929835003850d03
eddyizm <eddyizm@gmail.com>
2026-01-30 21:35:42 -0800

    chore: removed flet gui stuff

--- src/PythonCodeBase/DAL.py
+++ src/PythonCodeBase/DAL.py
@@ -1,115 +1,130 @@
-''' Data Access Layer
+""" Data Access Layer
 Main library to access database, read sql scripts, and generate files. 
 Will also use this area to check for ODBC.
-'''
+"""
+
 import pyodbc as db
 import pandas as pd
 import os
 from datetime import datetime
 import xlsxwriter
 import logging
-log = logging.getLogger('root')
+
+log = logging.getLogger("root")
 
 date_var = datetime.now().strftime("%Y%m%d")
-# When called from UI the directory will default to the l`ocation of the SQL script. 
+# When called from UI the directory will default to the l`ocation of the SQL script.
 # TODO figure out how to auto increment this and pass it/share it with c# codebase
-__version__= '0.1.5'
+__version__ = "0.1.5"
 
-DB_STRING = 'DRIVER={{{}}};SERVER={};Trusted_Connection=yes;DATABASE={}'	
-USERPASS_DBSTRING = 'DRIVER={{{}}};SERVER={};DATABASE={};UID={};PWD={}'
-ODBC_MISSING_MSG = '''ODBC driver not found
+DB_STRING = "DRIVER={{{}}};SERVER={};Trusted_Connection=yes;DATABASE={}"
+USERPASS_DBSTRING = "DRIVER={{{}}};SERVER={};DATABASE={};UID={};PWD={}"
+ODBC_MISSING_MSG = """ODBC driver not found
 please install ODBC Driver 17 for SQL Server
-https://www.microsoft.com/en-us/download/details.aspx?id=56567'''
-MSDRIVERS = ['ODBC Driver 17 for SQL Server', 'SQL Server Native Client 11.0', 'SQL Server']
+https://www.microsoft.com/en-us/download/details.aspx?id=56567"""
+MSDRIVERS = [
+    "ODBC Driver 17 for SQL Server",
+    "SQL Server Native Client 11.0",
+    "SQL Server",
+]
 
 
 def check_odbc() -> bool:
-  ''' check for odbc driver '''
-  try:
-    drivers = db.drivers()
-    if drivers:
-      log.info('Checking for a compatible driver')
-      for driver in MSDRIVERS:
-        if driver in drivers:
-          log.info(f'Driver found - {driver}')
-          return True
-      else:
-        log.warn(ODBC_MISSING_MSG)
-        return False    
-    else:
-      log.warn(ODBC_MISSING_MSG)
-      return False
-  except Exception as ex:
-    log.error(f'Error in check_odbc(): {ex}')
+    """check for odbc driver"""
+    try:
+        drivers = db.drivers()
+        if drivers:
+            log.info("Checking for a compatible driver")
+            for driver in MSDRIVERS:
+                if driver in drivers:
+                    log.info(f"Driver found - {driver}")
+                    return True
+            else:
+                log.warn(ODBC_MISSING_MSG)
+                return False
+        else:
+            log.warn(ODBC_MISSING_MSG)
+            return False
+    except Exception as ex:
+        log.error(f"Error in check_odbc(): {ex}")
 
 
 def get_odbc() -> str:
-  ''' get odbc driver - the check should have already been done and pass
-  however maybe I need to combine this later to specify which one to use.
-  '''
-  try:
-    drivers = db.drivers()
-    if drivers:
-      log.info('Loading driver')
-      for driver in MSDRIVERS:
-        if driver in drivers:
-          log.info(f'Driver found - {driver}')
-          return driver
+    """get odbc driver - the check should have already been done and pass
+    however maybe I need to combine this later to specify which one to use.
+    """
+    try:
+        drivers = db.drivers()
+        if drivers:
+            log.info("Loading driver")
+            for driver in MSDRIVERS:
+                if driver in drivers:
+                    log.info(f"Driver found - {driver}")
+                    return driver
 
-  except Exception as ex:
-    log.error(f'Error in get_odbc(): {ex}')
+    except Exception as ex:
+        log.error(f"Error in get_odbc(): {ex}")
 
 
 def read_file(file):
-  ''' method to read sql script. 
-  TODO Will add a check to validate that it has
-  SET NO COUNT in it '''
-  try:
-    log.info('Reading sql script from file.')
-    with open(file, 'r') as f:
-      _qry = f.read()
-      return _qry
-  except Exception as ex:
-    log.error(f'Error in read_file(): {ex}')
-    
+    """method to read sql script.
+    TODO Will add a check to validate that it has
+    SET NO COUNT in it"""
+    try:
+        log.info("Reading sql script from file.")
+        with open(file, "r") as f:
+            _qry = f.read()
+            return _qry
+    except Exception as ex:
+        log.error(f"Error in read_file(): {ex}")
 
-def generate_file(query, db_name, server, separator, directory, extension, file_name=None, username=None, password=None):
-  ''' generate file magic '''
-  log.info('Generating file')
-  driver = get_odbc()
-  # defaulting to no username and going to trusted connection. Should be the setup in most enterprise shops
-  if not username:
-    log.debug('No username received, using trusted connection (MSSQL)')
-    db_connection=DB_STRING.format(driver, server, db_name)
-  else:
-    db_connection = USERPASS_DBSTRING.format(driver, server, db_name, username, password)
-  # passing the name supplied otherwise defaulting but this may not work properly. Need to test in other environments.
-  if file_name:
-    filename = (file_name + '_' + date_var + extension)
-  else:
-    filename = os.path.join(directory, (f'ffe_output_{date_var}{extension}'))
-  # connect to db and execute query, output file
-  try:
-    log.info('Connecting to database server')
-    log.debug(f'DB_STRING: {db_connection}')
-    with db.connect(db_connection) as cnxn:
-        df = pd.read_sql((query), cnxn)
-        if extension == '.xlsx':
-          writer = pd.ExcelWriter(filename, engine = 'xlsxwriter')
-          df.to_excel(writer, sheet_name = (db_name), index = False)
-          writer.save()
-          writer.close()
-        else:
-          df.to_csv(filename ,sep = separator, encoding='utf-8', index = False)
-    return filename
-  except Exception as ex:
-    log.error(f'Error in generate_file() function: {ex}')
-  
 
-if __name__ == '__main__':
-  
-  pass
-  # TODO Add version 
-  
-  
-  
+def generate_file(
+    query,
+    db_name,
+    server,
+    separator,
+    directory,
+    extension,
+    file_name=None,
+    username=None,
+    password=None,
+):
+    """generate file magic"""
+    log.info("Generating file")
+    driver = get_odbc()
+    # defaulting to no username and going to trusted connection. Should be the setup in most enterprise shops
+    if not username:
+        log.debug("No username received, using trusted connection (MSSQL)")
+        db_connection = DB_STRING.format(driver, server, db_name)
+    else:
+        db_connection = USERPASS_DBSTRING.format(
+            driver, server, db_name, username, password
+        )
+    # passing the name supplied otherwise defaulting but this may not work properly. Need to test in other environments.
+    if file_name:
+        filename = file_name + "_" + date_var + extension
+    else:
+        filename = os.path.join(directory, (f"ffe_output_{date_var}{extension}"))
+    # connect to db and execute query, output file
+    try:
+        log.info("Connecting to database server")
+        log.debug(f"DB_STRING: {db_connection}")
+        with db.connect(db_connection) as cnxn:
+            df = pd.read_sql((query), cnxn)
+            if extension == ".xlsx":
+                writer = pd.ExcelWriter(filename, engine="xlsxwriter")
+                df.to_excel(writer, sheet_name=(db_name), index=False)
+                writer.save()
+                writer.close()
+            else:
+                df.to_csv(filename, sep=separator, encoding="utf-8", index=False)
+        return filename
+    except Exception as ex:
+        log.error(f"Error in generate_file() function: {ex}")
+
+
+if __name__ == "__main__":
+
+    pass
+    # TODO Add version
--- src/PythonCodeBase/flatfile_cli.py
+++ src/PythonCodeBase/flatfile_cli.py
@@ -1,4 +1,4 @@
-﻿''' Python script to be called from WPF C# applicaiton.
+﻿""" Python script to be called from WPF C# applicaiton.
     Compiling with pyinstaller to EXE.
     pyinstaller -F --icon="../ffe.ico" --clean --version-file=flatfile_cli.txt --distpath "../Resources" flatfile_cli.py
     
@@ -6,23 +6,26 @@
     # linux sql server - 127.0.0.1,14333
     # database - TutorialDB
    
-''' 
+"""
+
 import os
-import DAL as db # local library
+import DAL as db  # local library
 import argparse
 import logging
 from logging.handlers import RotatingFileHandler
 import sys
-directory = os.getcwd() 
+
+directory = os.getcwd()
 
 LOG_DIR = directory
-LOG_FILE = os.path.join(LOG_DIR, 'flatfile_cli.log') 
-FINISH_MSG = 'Closing FFE CLI'
-log_formatter = logging.Formatter('%(asctime)s | %(levelname)s | %(message)s')
-log_handler = RotatingFileHandler(LOG_FILE, mode='a', maxBytes=5*1024*1024, 
-                                 backupCount=5, encoding=None, delay=0)
+LOG_FILE = os.path.join(LOG_DIR, "flatfile_cli.log")
+FINISH_MSG = "Closing FFE CLI"
+log_formatter = logging.Formatter("%(asctime)s | %(levelname)s | %(message)s")
+log_handler = RotatingFileHandler(
+    LOG_FILE, mode="a", maxBytes=5 * 1024 * 1024, backupCount=5, encoding=None, delay=0
+)
 log_handler.setFormatter(log_formatter)
-log = logging.getLogger('root')
+log = logging.getLogger("root")
 log.setLevel(logging.DEBUG)
 log.addHandler(log_handler)
 ch = logging.StreamHandler()
@@ -32,84 +35,135 @@ log.addHandler(ch)
 
 
 # argument parser
-parser = argparse.ArgumentParser(description="Reads a sql script or calls a stored procedure and exports results to a csv, txt or excel file.")
-parser.add_argument("-V", "--version", action='version', version =f'{db.__version__}')
+parser = argparse.ArgumentParser(
+    description="Reads a sql script or calls a stored procedure and exports results to a csv, txt or excel file."
+)
+parser.add_argument("-V", "--version", action="version", version=f"{db.__version__}")
 parser.add_argument("server", help="server to run query against.")
 parser.add_argument("db", help="database to use on server.")
-parser.add_argument("directory", help="location to export flat file. do not specify extension.")
+parser.add_argument(
+    "directory", help="location to export flat file. do not specify extension."
+)
 # optionals
-parser.add_argument("-f", "--filename", help="filename. Date will be appended.", required=False)
-parser.add_argument("-u", "--username", help="username for db connection.", default=None, required=False)
-parser.add_argument("-pass", "--password", help="use login/pass from instead of default Integrated Security (Trusted_Connection).", default=None, required=False)
+parser.add_argument(
+    "-f", "--filename", help="filename. Date will be appended.", required=False
+)
+parser.add_argument(
+    "-u", "--username", help="username for db connection.", default=None, required=False
+)
+parser.add_argument(
+    "-pass",
+    "--password",
+    help="use login/pass from instead of default Integrated Security (Trusted_Connection).",
+    default=None,
+    required=False,
+)
 # real first group for extension
 group_extension = parser.add_mutually_exclusive_group(required=True)
-group_extension.add_argument("-csv", "--csv", action='store_const', const='.csv', help="output file with csv extension.")
-group_extension.add_argument("-txt", "--txt", action='store_const', const='.txt', help="output file with txt extension.")
-group_extension.add_argument("-xlsx", "--xlsx", action='store_const', const='.xlsx', help="output excel file.")
+group_extension.add_argument(
+    "-csv",
+    "--csv",
+    action="store_const",
+    const=".csv",
+    help="output file with csv extension.",
+)
+group_extension.add_argument(
+    "-txt",
+    "--txt",
+    action="store_const",
+    const=".txt",
+    help="output file with txt extension.",
+)
+group_extension.add_argument(
+    "-xlsx", "--xlsx", action="store_const", const=".xlsx", help="output excel file."
+)
 # first group for seperator
 group_seperator = parser.add_mutually_exclusive_group(required=True)
-group_seperator.add_argument("-c", "--comma", action='store_const', const=',', help="use comma character for seperator.")
-group_seperator.add_argument("-t", "--tab", action='store_const', const='\t', help="use tab for seperator.")
-group_seperator.add_argument("-p", "--pipe", action='store_const', const='|', help="use pipe character for seperator.")
+group_seperator.add_argument(
+    "-c",
+    "--comma",
+    action="store_const",
+    const=",",
+    help="use comma character for seperator.",
+)
+group_seperator.add_argument(
+    "-t", "--tab", action="store_const", const="\t", help="use tab for seperator."
+)
+group_seperator.add_argument(
+    "-p",
+    "--pipe",
+    action="store_const",
+    const="|",
+    help="use pipe character for seperator.",
+)
 # second group for sql script or stored proc
 group_sql = parser.add_mutually_exclusive_group(required=True)
-group_sql.add_argument("-s", "--sqlscript",  help="full path of sql script *MUST HAVE* SET NO COUNT on script.")
-group_sql.add_argument("-sp", "--storedproc", help="instead of sql script, point to stored procedure on database. Wrap in double quotes with parameters eg. \"EXEC SAMPLE_SP '<Param>'\"")
+group_sql.add_argument(
+    "-s",
+    "--sqlscript",
+    help="full path of sql script *MUST HAVE* SET NO COUNT on script.",
+)
+group_sql.add_argument(
+    "-sp",
+    "--storedproc",
+    help="instead of sql script, point to stored procedure on database. Wrap in double quotes with parameters eg. \"EXEC SAMPLE_SP '<Param>'\"",
+)
 
-args=parser.parse_args()
-current_directory= os.getcwd() # this may change before beta/alpha testing.
+args = parser.parse_args()
+current_directory = os.getcwd()  # this may change before beta/alpha testing.
+
 
 def clean_path(dir_path):
-    return dir_path.replace('\\', '/')
+    return dir_path.replace("\\", "/")
 
 
 def get_seperator():
-    ''' find argument with value from GUI or CLI '''
+    """find argument with value from GUI or CLI"""
     # TODO test against DB. the \t might work directly from the constant
-    log.info('Getting separator...')
+    log.info("Getting separator...")
     if args.comma:
         return args.comma
     elif args.tab:
         return args.tab
     else:
-        return args.pipe    
+        return args.pipe
 
 
 def get_extension():
-    ''' find extension with value from GUI or CLI '''
+    """find extension with value from GUI or CLI"""
     # TODO test against DB. the \t might work directly from the constant
-    log.info('Getting extenstion...')
+    log.info("Getting extenstion...")
     if args.csv:
         return args.csv
     elif args.txt:
         return args.txt
     else:
-        return args.xlsx        
+        return args.xlsx
 
 
 def log_args():
-    ''' CLI arg inputs -debugging method only'''
-    log.debug(f'args.server: {args.server}')
-    log.debug(f'args.db: {args.db}')
-    log.debug(f'args.directory: {args.directory}')
-    log.debug(f'args.filename: {args.filename}')
-    log.debug(f'args.comma: {args.comma}')
-    log.debug(f'args.tab: {args.tab}')
-    log.debug(f'args.pipe: {args.pipe}')
-    log.debug(f'args.sqlscript: {args.sqlscript}')
-    log.debug(f'args.storedproc: {args.storedproc}')
-    log.debug(f'args.username: {args.username}')
-    log.debug(f'args.password: {args.password}')
-    
+    """CLI arg inputs -debugging method only"""
+    log.debug(f"args.server: {args.server}")
+    log.debug(f"args.db: {args.db}")
+    log.debug(f"args.directory: {args.directory}")
+    log.debug(f"args.filename: {args.filename}")
+    log.debug(f"args.comma: {args.comma}")
+    log.debug(f"args.tab: {args.tab}")
+    log.debug(f"args.pipe: {args.pipe}")
+    log.debug(f"args.sqlscript: {args.sqlscript}")
+    log.debug(f"args.storedproc: {args.storedproc}")
+    log.debug(f"args.username: {args.username}")
+    log.debug(f"args.password: {args.password}")
+
 
 def get_login():
-    ''' assign login credentials if passed from GUI or from CLI '''
+    """assign login credentials if passed from GUI or from CLI"""
     if args.username:
-        log.debug('Getting credentials')
+        log.debug("Getting credentials")
         return args.username, args.password
     else:
-        log.debug('no credentials found, returning None')
-        return None, None        
+        log.debug("no credentials found, returning None")
+        return None, None
 
 
 def get_filename():
@@ -117,39 +171,57 @@ def get_filename():
     # TODO pass filename or generate default here if none is pass
     # since i didn't fucking make it required in the args. DUMB DUMB!
 
+
 def main():
     # odbc check
     if not db.check_odbc():
         log.warn("Try again after installing ODBC driver. Application exiting.")
         os.system("pause")
         sys.exit()
-    
+
     login, cred = get_login()
-    
+
     try:
         if not args.sqlscript:
-            ''' if sql script is None/null then process using stored proc '''
-            log.info(f'executing stored proc {args.storedproc}') 
+            """if sql script is None/null then process using stored proc"""
+            log.info(f"executing stored proc {args.storedproc}")
             # fullpath = os.path.join(args.directory, args.filename)
-            result = db.generate_file(args.storedproc, args.db, args.server, get_seperator(), args.directory, get_extension(), username=login, password=cred)
+            result = db.generate_file(
+                args.storedproc,
+                args.db,
+                args.server,
+                get_seperator(),
+                args.directory,
+                get_extension(),
+                username=login,
+                password=cred,
+            )
         else:
-            log.info(f'executing sql script {args.sqlscript}')
+            log.info(f"executing sql script {args.sqlscript}")
             qry = db.read_file(args.sqlscript)
-            
-            result = db.generate_file(qry, args.db, args.server, get_seperator(), clean_path(args.directory), get_extension(), username=login, password=cred)
-    except Exception as ex:
-        log.error(f'error in main(): {ex}')
-    finally: 
-        if result:
-            log.info(f'file generated - {result}')
-        else:
-            log.info(f'No file generated. Please check the logs')
-        os.system("pause")      
-    
 
-if __name__ == '__main__':
-    log.info(f'Startiing FFE CLI version: {db.__version__}')
+            result = db.generate_file(
+                qry,
+                args.db,
+                args.server,
+                get_seperator(),
+                clean_path(args.directory),
+                get_extension(),
+                username=login,
+                password=cred,
+            )
+    except Exception as ex:
+        log.error(f"error in main(): {ex}")
+    finally:
+        if result:
+            log.info(f"file generated - {result}")
+        else:
+            log.info(f"No file generated. Please check the logs")
+        os.system("pause")
+
+
+if __name__ == "__main__":
+    log.info(f"Startiing FFE CLI version: {db.__version__}")
     # log_args()
     main()
     log.info(FINISH_MSG)
-    
